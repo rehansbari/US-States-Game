fe892b2658ed7edd9ced61ef165ce41ea38e346f
Rehan Bari <rehansbari@gmail.com>
2022-12-15 10:50:55 -0700

    Changed for loop to use list comprehension

--- main.py
+++ main.py
@@ -16,16 +16,13 @@ guessed_states = []
 
 #Main game functionality, will run untill the user guesses all 50 states
 #Game will check if the answer a user provides exists in a list of states extracted from a CSV file
-#If player guesses correctly a turtle object will write the name of the state on the map and add their score to a list of correctly guessed states. 
+#If player guesses correctly a turtle object will write the name of the state on the map and add their score to a list of correctly guessed states.
 #Game will generate a CSV file of all the states not guessed if user decides to leave the game early
 while len(guessed_states) < 50:
     user_answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
 
     if user_answer == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
@@ -37,4 +34,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == user_answer]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(user_answer)
-        print("Good Job")
